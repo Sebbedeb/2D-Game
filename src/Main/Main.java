a118b29c4476ea88064caaaeac05c7d61c18fbfd
Sebbedeb <zor.seb.0901@gmail.com>
2022-11-30 10:39:08 +0100

    Header ændret

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -7,7 +7,7 @@ public class Main {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
-        window.setTitle("VERONIKA ER LÆKKER");
+        window.setTitle("2D game");
 
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
